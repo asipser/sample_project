ed2df14b17d1128de722990f11308d3cc7af64bd
Slava Kim <slv@mit.edu>
2016-01-08 10:22:32 -0500

    prepare for mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-
-// Database in Memory
-var db = {
-	'david': 'apple',
-	'monde': 'orange',
-	'alan': 'pineapple',
-	'liang': 'banana'
-}
+// Access to real DB
+var db = require('../db-setup.js');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -20,57 +14,57 @@ router.get('/', function (req, res, next) {
 
 /* GET userlist JSON */
 router.get('/userlist', function (req, res, next) {
-
-	// Sending the db object
-	res.send(db);
-
+  // TODO: query database db.users.find(...) and return the result
+  // res.send({...});
+  res.send('Nothing :(');
 });
 
 /* POST to adduser */
 router.post('/adduser', function (req, res, next) {
 
-	// Catching variables passed in the form
-	var userName = req.body.username;
-	var userFruit = req.body.userfruit;
+  // Catching variables passed in the form
+  var userName = req.body.username;
+  var userFruit = req.body.userfruit;
 
-	// Adding the new entry to the db
-	db[userName] = userFruit;
+  // Adding the new entry to the db
+  // TODO: insert the new document into collection
+  //   db.users.insert({ ... });
+  //      ... or ...
+  // update the existing one!
+  //   db.users.update({ _id: ... }, { $set: {...} })
+  // How would you do this?
 
-	// Redirecting back to the root
-	res.redirect('/');
+  // Redirecting back to the root
+  res.redirect('/');
 
 });
 
 /* POST to deleteuser */
 router.post('/deleteuser', function (req, res, next) {
 
-	// Catching variables passed in the form
-	var userName = req.body.username;
+  // Catching variables passed in the form
+  var userName = req.body.username;
 
-	// Checking whether user is in db
-	if (userName in db) {
-
-		// If yes, user is deleted from db
-		delete db[userName]
-		res.redirect('/');	
-	} else {
-
-		// If not, error message is rendered
-		var err = {
-			message: "User not found",
-		};
-		res.render('error', err);
-	}
+  // Checking whether user is in db
+  // TODO: check if the user is in db with db.users.find(...),
+  // TODO: and then remove it if it exists db.users.remove(...)
+  // TODO: or return an error
+  res.send('Nothing :(');
 });
 
 router.get('/findfruit', function (req, res, next) {
-	var name = req.query.username;
-	if (db[name]) {
-		res.send(db[name]);
-	}
-	else {
-		res.send("Error! Username not found");
-	}
+  var name = req.query.username;
+
+  // TODO: check if user exists. If user exists, send back their fruit.
+  
+  var userExists = false;
+  if (userExists) {
+    res.send('Nothing :(');
+    //res.send({});
+  }
+  else {
+    res.send("Error! Username not found");
+  }
 });
 
 module.exports = router;
