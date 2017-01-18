e3034cf730e4778c029e7c5effb2ebe9fed914ef
Aaron Sipser <ajsipser@gmail.com>
2017-01-18 00:23:15 -0500

    azure2

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var routes = require('./routes/index');
 var app = express();
 
 // database setup
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/test');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://aaronsmongodbrw:xSJEozk4Tmg74Q1iyXMN0sEgr0PfegnIrDz5xq8N5UvmwlsFSSqGR0QMAx1nw5hdiENdcSQbHHK7t4ZQY0wf6g==@aaronsmongodbrw.documents.azure.com:10250/test?ssl=true');
 var connection = mongoose.connection;
 connection.on('error', console.error.bind(console, 'connection error:'));
 connection.on('connected', function() {
